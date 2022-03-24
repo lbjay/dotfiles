5c00e164310328fc80084cf95efc5626046afb07
Jay Luker <jay_luker@harvard.edu>
2022-03-24 12:02:46 -0400

    initial redo; removing everything but the dotfiles for now

--- Makefile
+++ Makefile
@@ -1,62 +1,17 @@
 #!/bin/bash
-#
-# Running `make install` will run all the below tasks installing everything.
-# Alternatively you can choose what parts to install yourself.
-#
-# Add new functionality by adding a topic folder e.g. ./nodejs and in it an
-# install.sh script which handles all aspects of that topic. It will be
-# run automatically by `make topics` below.
-#
-# Any topic folder containing a file called *.symlink will be symlinked
-# automatically to the home folder by `make symlink`, see the README.md
-# for more details.
-#
 
 SHELL := /bin/bash
 DOTFILES_ROOT := `pwd -P`
 
-.PHONY: help file_system install symlink dotfiles brew homebrew topics run
+.PHONY: help dotfiles
 
 help:
 	@echo ""
 	@echo "Dotfiles"
 	@echo "--------"
 	@echo ""
-	@echo "install     - Installs file_system, dotfiles, homebrew and topics."
-	@echo "file_system - Creates all necessary files and directories."
-	@echo "dotfiles    - Installs dotfile symlinks."
-	@echo "brew        - Installs homebrew packages."
-	@echo "topics      - Runs all topic installers."
-	@echo "run         - Runs the given topic installer."
-	@echo "              Use 'make run TOPIC=ruby' etc."
-	@echo "              Note: some installers require certain brew packages."
+	@echo "dotfiles - Installs dotfile symlinks."
 	@echo ""
 
-install: file_system symlink brew topics
-    # Wrapper for dependant tasks.
-
-file_system:
-	@source "$(DOTFILES_ROOT)/install_helpers.sh" && \
-	source "$(DOTFILES_ROOT)/file_system/install.sh"
-
-symlink:
-	@source "$(DOTFILES_ROOT)/install_helpers.sh" && \
-	setup_gitconfig && \
-	symlink_dotfiles
-dotfiles: symlink
-    # Alias for symlink.
-
-brew:
-	@source "$(DOTFILES_ROOT)/install_helpers.sh" && \
-	source "$(DOTFILES_ROOT)/homebrew/install.sh"
-homebrew: brew
-    # Alias for brew.
-
-topics:
-	@source "$(DOTFILES_ROOT)/install_helpers.sh" && \
-	find $(DOTFILES_ROOT) -maxdepth 2 -type f -name 'install.sh' \
-		-not -path $(DOTFILES_ROOT)/homebrew/install.sh -exec echo {} \;
-
-run:
-	@source "$(DOTFILES_ROOT)/install_helpers.sh" && \
-	source "$(DOTFILES_ROOT)/$(TOPIC)/install.sh"
+dotfiles:
+	@source "$(DOTFILES_ROOT)/install_helpers.sh" && symlink_dotfiles
--- install_helpers.sh
+++ install_helpers.sh
@@ -10,64 +10,39 @@ set -e
 
 export DOTFILES_ROOT=$(pwd -P)
 
-info () {
+info() {
   printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
-user () {
+user() {
   printf "\r  [ \033[0;33m??\033[0m ] $1\n"
 }
 
-success () {
+success() {
   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
 
-fail () {
+fail() {
   printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
   echo ''
   exit 1
 }
 
-setup_gitconfig () {
-  if ! [ -f git/gitconfig.local.symlink ]
-  then
-    info 'Initialising git configuration...'
-
-    git_credential='cache'
-    if [ "$(uname -s)" == "Darwin" ]
-    then
-      git_credential='osxkeychain'
-    fi
-
-    user ' - What is your full name (not your GitHub handle)?'
-    read -e git_authorname
-    user ' - What is your github author email?'
-    read -e git_authoremail
-
-    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
-
-    success 'Git configuration complete'
-  fi
-}
-
-link_file () {
+link_file() {
   local src=$1 dst=$2
 
   local overwrite= backup= skip=
   local action=
 
-  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
-  then
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
 
-    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
-    then
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
 
       local currentSrc="$(readlink $dst)"
 
-      if [ "$currentSrc" == "$src" ]
-      then
+      if [ "$currentSrc" == "$src" ]; then
 
-        skip=true;
+        skip=true
 
       else
 
@@ -76,20 +51,26 @@ link_file () {
         read -n 1 action
 
         case "$action" in
-          o )
-            overwrite=true;;
-          O )
-            overwrite_all=true;;
-          b )
-            backup=true;;
-          B )
-            backup_all=true;;
-          s )
-            skip=true;;
-          S )
-            skip_all=true;;
-          * )
-            ;;
+        o)
+          overwrite=true
+          ;;
+        O)
+          overwrite_all=true
+          ;;
+        b)
+          backup=true
+          ;;
+        B)
+          backup_all=true
+          ;;
+        s)
+          skip=true
+          ;;
+        S)
+          skip_all=true
+          ;;
+        *) ;;
+
         esac
 
       fi
@@ -100,17 +81,14 @@ link_file () {
     backup=${backup:-$backup_all}
     skip=${skip:-$skip_all}
 
-    if [ "$overwrite" == "true" ]
-    then
+    if [ "$overwrite" == "true" ]; then
       rm -rf "$dst"
       success "Removed $dst"
     fi
 
-    if [ "$backup" == "true" ]
-    then
+    if [ "$backup" == "true" ]; then
       # Only backup if the $dst file isn't a symlink.
-      if [ -L "$dst" ]
-      then
+      if [ -L "$dst" ]; then
         rm -f "$dst"
       else
         mv "$dst" "${dst}.backup"
@@ -118,50 +96,26 @@ link_file () {
       fi
     fi
 
-    if [ "$skip" == "true" ]
-    then
+    if [ "$skip" == "true" ]; then
       success "Skipped $src"
     fi
   fi
 
-  if [ "$skip" != "true" ]  # "false" or empty
-  then
+  if [ "$skip" != "true" ]; then # "false" or empty
     ln -s "$1" "$2"
     success "Sym linked $1 to $2"
   fi
 }
 
-symlink_dotfiles () {
+symlink_dotfiles() {
   info "Symlinking dotfiles..."
 
   local overwrite_all=false backup_all=true skip_all=false
 
-  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
-  do
+  for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*'); do
     dst="$HOME/.$(basename "${src%.*}")"
     link_file "$src" "$dst"
   done
 
   success "Symlinking dotfiles complete"
-}
-
-create_dir () {
-  if [ ! -d "${1}" ]; then
-    info "Creating directory: ${1}"
-    mkdir -p "${1}"
-  fi
-}
-
-create_file () {
-  if [ ! -f "${1}" ]; then
-    info "Creating file: ${1}"
-    touch "${1}"
-    if [ $# -eq 2 ]; then
-      echo "${2}" >> "${1}"
-    fi
-  fi
-}
-
-installed () {
-  !(test ! $(which $1))
 }
